a7e1ac292dab0cad222a00efa8a435663db8568e
Evandson César de Arruda Santiago <evandson.cesar@gmail.com>
2025-08-19 19:25:36 -0300

    Fix: Corrige configuração para deploy no GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import DashboardPage from './pages/DashboardPage';
-import DashboardDetails from './pages/DashboardDetails';
-import Socioeconomico from './pages/Socioeconomico';
-import Empreendimentos from './pages/Empreendimentos';
-import Mapa from './pages/Mapa';
-import Planos from './pages/Planos';
-import Configuracoes from './pages/Configuracoes';
-import DetalhesEmpreendimento from './pages/DetalhesEmpreendimento';
-import Login from './pages/Login';
-import EsqueciSenha from './pages/EsqueciSenha';
-import Cadastro from './pages/Cadastro';
-import { JSX } from 'react';
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import DashboardPage from "./pages/DashboardPage";
+import DashboardDetails from "./pages/DashboardDetails";
+import Socioeconomico from "./pages/Socioeconomico";
+import Empreendimentos from "./pages/Empreendimentos";
+import Mapa from "./pages/Mapa";
+import Planos from "./pages/Planos";
+import Configuracoes from "./pages/Configuracoes";
+import DetalhesEmpreendimento from "./pages/DetalhesEmpreendimento";
+import Login from "./pages/Login";
+import EsqueciSenha from "./pages/EsqueciSenha";
+import Cadastro from "./pages/Cadastro";
+import { JSX } from "react";
 
 function App(): JSX.Element {
   return (
@@ -20,7 +20,10 @@ function App(): JSX.Element {
         <Route path="/dashboarddetails" element={<DashboardDetails />} />
         <Route path="/socioeconomico" element={<Socioeconomico />} />
         <Route path="/empreendimentos" element={<Empreendimentos />} />
-        <Route path="/empreendimentos/:id" element={<DetalhesEmpreendimento />} />
+        <Route
+          path="/empreendimentos/:id"
+          element={<DetalhesEmpreendimento />}
+        />
         <Route path="/mapa" element={<Mapa />} />
         <Route path="/planos" element={<Planos />} />
         <Route path="/configuracoes" element={<Configuracoes />} />
